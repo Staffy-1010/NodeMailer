791f78b2e6dbb382a48a016d9f93526635e724d3
Staffy <72374474+Staffy-1010@users.noreply.github.com>
2021-05-11 20:58:11 +0530

    Add files via upload

---
+++ routers/userrouter.js
@@ -0,0 +1,70 @@
+var express=require("express");
+var path=require("path");
+var nodemailer=require("nodemailer");
+var mysql=require("mysql");
+var config={
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"nodemailer"
+}
+var dbcon=mysql.createConnection(config)
+var app=express();
+app.use(express.static("public"));
+var transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    service: "gmail",
+    auth: {
+      user: 'staffy7508@gmail.com', 
+      pass: '*********' 
+    },
+  });
+app.post("/sendmail",(req,res)=>{
+    var {too,sub,msg}=req.body;
+    var info =transporter.sendMail({
+        from: 'staffy7508@gmail.com',
+        to: too,
+        subject: sub,
+        text: msg ,
+      });
+    dbcon.query("insert into mails values(?,?,?,CURRENT_DATE(),CURRENT_TIME())",[too,sub,msg],(err,result)=>{
+        if(err)
+        res.end(err.message);
+        else{
+        var fullPath=path.join(__dirname,"..","public","index.html");
+        res.sendFile(fullPath);
+        }
+    })
+  })
+  app.post("/all",(req,res)=>{
+    var {too,sub,msg}=req.body;
+    dbcon.query("insert into mails values(?,?,?,CURRENT_DATE(),CURRENT_TIME())",[too,sub,msg]);
+    dbcon.query("select * from mails",(err,result)=>{
+        if(err)
+        res.end(err.message);
+        else{
+        var arr=result;
+        arr.forEach(x => {
+          console.log(x.too);
+          transporter.sendMail({
+              from: 'staffy7508@gmail.com',
+              to: x.too,
+              subject: sub,
+              text: msg ,
+            })
+        });
+        var fullPath=path.join(__dirname,"..","public","index.html");
+        res.sendFile(fullPath);
+        }
+    })
+  })
+app.get("/mails",(req,res)=>{
+      dbcon.query("select * from mails",(err,result)=>{
+          if(err)
+          res.end(err.message);
+          else
+          res.send(result);
+      })
+  })
+module.exports=app;
